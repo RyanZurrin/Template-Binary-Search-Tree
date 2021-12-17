f23b90204081127f6a2f6edee049f5a2283847ab
Ryan Zurrin <rzurrin@gmail.com>
2021-12-17 00:43:28 -0500

    fixed move semantics

--- Template Binary Search Tree/driver.cpp
+++ Template Binary Search Tree/driver.cpp
@@ -4,32 +4,26 @@
 int main() {
 	TBST<int>* tree = new TBST<int>(100);
 
-	tree->addItem(7);
-	tree->addItem(20);
 	tree->addItem(10);
-	tree->addItem(17);
-	tree->addItem(8);
-	tree->addItem(26);
+	tree->addItem(5);
 	tree->addItem(11);
-	tree->addItem(1);
-	tree->addItem(15);
 	tree->addItem(3);
-	tree->addItem(13);
-	tree->addItem(2);
+	tree->addItem(16);
+	tree->addItem(6);
+	tree->addItem(7);
+	tree->addItem(0);
+
+	
 
 	tree->traverseTree(TBST<int>::OrderType::PRE_ORDER);
 
-	tree->traverseTree(TBST<int>::OrderType::BREADTH_FIRST);
+	tree->traverseTree(TBST<int>::OrderType::IN_ORDER);
 
 	tree->traverseTree(TBST<int>::OrderType::POST_ORDER);
-
-	std::cout << tree->rankOf(tree->returnRoot(), 13) << std::endl;
-
-	std::cout << tree->getCount() << std::endl;
-	std::cout << tree->getQty() << std::endl;
 
 
 
 
 	return EXIT_SUCCESS;
 }
+
